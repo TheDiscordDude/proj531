391d33efe30bdd131101bf69aa91bc020394557c
Boughanmi Rami <rami.boughanmi@etu.u-pec.fr>
2021-12-16 11:42:35 +0100

    update import

--- src/main.py
+++ src/main.py
@@ -1,29 +1,19 @@
 import random
-
+from display import *
 import chess
-
-class COLORS:
-    WHITE = '\033[96m'
-    BLACK = '\033[94m'
-    ENDC = '\033[0m'
-
 board = chess.Board()
-
-def showBoard(board):
-    nstr = ""
-    for i in str(board):
-        if i in [".", "\n", " "]:
-            nstr+=i
-        elif i.islower():
-            nstr+=COLORS.BLACK + i + COLORS.ENDC
-        elif i.isupper():
-            nstr+=COLORS.WHITE + i + COLORS.ENDC
-    
-    print(nstr)
 #print(board.legal_moves)
-move = chess.Move(chess.A2, chess.A3)
-board.push(move)
-print(board.turn)
-showBoard(board)
+Nf3 = chess.Move.from_uci("a2a4")
+Nf4 = chess.Move.from_uci("a7a6")
+display_board(board)
 
+#move = chess.Move(chess.A3, chess.A3)
+#board.push(move)
+board.push(Nf3)
+print('is cap :', board.is_capture(Nf3))
+board.push(Nf4)
+board.turn = False
+print('board turn :', board.turn)
+print(board.legal_moves.count())
+display_board(board)
 
